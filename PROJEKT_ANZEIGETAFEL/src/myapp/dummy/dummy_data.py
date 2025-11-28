9c8606c2fa0051e7dcc1b79c6df88477c980cb1a
Julian Pavek <jpavek@student.tgm.ac.at>
2025-11-28 08:33:50 +0100

    Update dummy data for teams and players

--- PROJEKT_ANZEIGETAFEL/src/myapp/dummy/dummy_data.py
+++ PROJEKT_ANZEIGETAFEL/src/myapp/dummy/dummy_data.py
@@ -2,14 +2,15 @@ from models.team import Team
 from models.player import Player
 
 TEAMS = [
-    Team(id=1, name="FC Lions"),
-    Team(id=2, name="Raptors United")
+    Team(id=1, name="Austria Wien"),
+    Team(id=2, name="Rapid Wien")
 ]
 
 PLAYERS = [
-    Player(id=1, team_id=1, vorname="Lukas", nachname="Meier", nummer=1, bild_url="https://picsum.photos/100?1"),
-    Player(id=2, team_id=1, vorname="Jonas", nachname="Keller", nummer=2, bild_url="https://picsum.photos/100?2"),
+    Player(id=1, team_id=1, vorname="Dejan", nachname="Radonjic", nummer=60, bild_url="https://picsum.photos/100?1"),
+    Player(id=2, team_id=1, vorname="Nikolas", nachname="Wurmbrand", nummer=40, bild_url="https://picsum.photos/100?2"),
     # … bis 40 Spieler
 
 ]
+
 
