dbb5ed80be0bc971d47823c6fdd22a0b4586f3bd
Fabienne <fbauer@student.tgm.ac.at>
2025-12-23 17:51:03 +0100

    Update player.py

--- PROJEKT_ANZEIGETAFEL/src/myapp/models/player.py
+++ PROJEKT_ANZEIGETAFEL/src/myapp/models/player.py
@@ -1,10 +1,7 @@
-from pydantic import BaseModel
-
-
-class Player(BaseModel):
-    id: int
-    team_id: int
-    vorname: str
-    nachname: str
-    nummer: int
-    bild_url: str
+class Player:
+    def __init__(self, name, nummer):
+        self.name = name
+        self.nummer = nummer
+        self.tore = 0
+        self.yellow_cards = 0
+        self.is_on_field = True  # Startspieler = True, Bank = False
