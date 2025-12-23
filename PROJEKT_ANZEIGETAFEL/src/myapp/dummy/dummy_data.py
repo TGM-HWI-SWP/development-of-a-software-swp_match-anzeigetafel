3652f388e616af506b2b06fe9df3857e8016e982
Fabienne <fbauer@student.tgm.ac.at>
2025-12-23 17:50:33 +0100

    Update dummy_data.py

--- PROJEKT_ANZEIGETAFEL/src/myapp/dummy/dummy_data.py
+++ PROJEKT_ANZEIGETAFEL/src/myapp/dummy/dummy_data.py
@@ -1,13 +1,59 @@
-from models.team import Team
-from models.player import Player
+import random
 
-TEAMS = [
-    Team(id=1, name="FC Lions"),
-    Team(id=2, name="Raptors United")
-]
+class Player:
+    def __init__(self, name, nummer):
+        self.name = name
+        self.nummer = nummer
+        self.tore = 0
+        self.yellow_cards = 0
 
-PLAYERS = [
-    Player(id=1, team_id=1, vorname="Lukas", nachname="Meier", nummer=1, bild_url="https://picsum.photos/100?1"),
-    Player(id=2, team_id=1, vorname="Jonas", nachname="Keller", nummer=2, bild_url="https://picsum.photos/100?2"),
-    # … bis 40 Spieler
-]
+class Team:
+    def __init__(self, name):
+        self.name = name
+        self.players = self.generate_players()
+
+    def generate_players(self):
+        # 11 auf Feld + 5 Bank
+        players = []
+        for i in range(1, 17):
+            players.append(Player(f"Spieler {i}", i))
+        return players
+
+    def get_field_players(self):
+        return self.players[:11]
+
+    def get_bench_players(self):
+        return self.players[11:]
+
+    def random_yellow_card(self):
+        player = random.choice(self.get_field_players())
+        player.yellow_cards += 1
+        return player
+
+    def random_substitution(self):
+        field = self.get_field_players()
+        bench = self.get_bench_players()
+        if not bench:
+            return None, None
+        out_player = random.choice(field)
+        in_player = random.choice(bench)
+        # Tausch
+        idx_out = field.index(out_player)
+        idx_in = self.players.index(in_player)
+        self.players[idx_out], self.players[idx_in] = self.players[idx_in], self.players[idx_out]
+        return out_player, in_player
+
+class DummyDB:
+    def __init__(self):
+        self.teams = []
+
+    def create_team(self, name):
+        team = Team(name)
+        self.teams.append(team)
+        return team
+
+    def get_team_by_name(self, name):
+        for team in self.teams:
+            if team.name == name:
+                return team
+        return None
