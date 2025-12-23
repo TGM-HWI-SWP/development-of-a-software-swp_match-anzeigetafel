34e062e478a936115ff4dba9617ee52511bf6aeb
Fabienne <fbauer@student.tgm.ac.at>
2025-12-23 16:42:40 +0100

    Update view_dummy.py

--- PROJEKT_ANZEIGETAFEL/src/myapp/adapter/view_dummy.py
+++ PROJEKT_ANZEIGETAFEL/src/myapp/adapter/view_dummy.py
@@ -1,21 +1,7 @@
-from contracts.ports import ViewPort
-from typing import List
-from models.team import Team
-from models.player import Player
+class Team:
+    def __init__(self, name):
+        self.name = name
 
-
-class DummyView(ViewPort):
-    """CLI-Version der GUI zum Testen."""
-
-    def show_teams(self, teams: List[Team]) -> None:
-        print("TEAMS:")
-        for t in teams:
-            print(f"- {t.name}")
-
-    def show_players(self, players: List[Player]) -> None:
-        print("SPIELER:")
-        for p in players:
-            print(f"{p.vorname} {p.nachname} (#{p.nummer})")
-
-    def show_score(self, home: int, away: int) -> None:
-        print(f"Spielstand: {home} : {away}")
+class DummyDB:
+    def get_teams(self):
+        return [Team("Team A"), Team("Team B"), Team("Team C")]
